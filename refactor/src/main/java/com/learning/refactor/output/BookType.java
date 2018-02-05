59c3c2403717b6f5bcc3c0139c2ef710e992baf0
santoshkiiit <santosh.k@imaginea.com>
2018-02-05 14:10:08 +0530

    refactor commie

---
+++ refactor/src/main/java/com/learning/refactor/output/BookType.java
@@ -0,0 +1,7 @@
+package com.learning.refactor.output;
+
+
+public enum BookType {
+
+    CHILDEN, FICTION, NONFICTION
+}
---
+++ refactor/src/main/java/com/learning/refactor/output/ChildrenBook.java
@@ -0,0 +1,39 @@
+package com.learning.refactor.output;
+
+import java.util.Date;
+
+
+public class ChildrenBook extends Book {
+
+
+    private static final long serialVersionUID = -6354671528L;
+    private static int THRESHOLD = 1;
+    private static final int  POINTS=1;
+    private static final double BOOKPRICE=1.5;
+
+
+
+
+    public ChildrenBook(Long id, String title, Date releaseDate) {
+        super(id, title, releaseDate);
+    }
+
+    @Override
+    public double getPrice() {
+        return BOOKPRICE;
+    }
+
+
+    @Override
+    public int getThreshold() {
+        return this.THRESHOLD;
+    }
+
+    @Override
+    public int getPoints() {
+        return this.POINTS;
+    }
+
+
+
+}
